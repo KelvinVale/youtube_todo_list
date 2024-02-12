a07b99821f9bb0aed69ede9446b354f5b94835eb
Kelvin Vale <kelvin.vale@ee.ufcg.edu.br>
2024-02-12 01:07:08 +0100

    TextField/TextEditingController/TextButton/setState

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -8,16 +8,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController textController = TextEditingController(text: "");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My title"), 
+      appBar: AppBar(title: const Text("My title"), 
       backgroundColor: Colors.amber,),
-      body: Column(children: [
-        Text("data1"),
-        Text("data1"),
-        Text("data1"),
-        Text("data1"),
-      ],),);
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(children: [
+              Text(textController.text),
+              Text(textController.text),
+              Text(textController.text),
+              Text(textController.text),
+              TextField(controller: textController, onChanged: (value) {
+                setState(() {});
+              },),
+            ],
+            ),
+            TextButton(onPressed: (){
+              setState(() {
+                textController.text = "";
+              });
+            }, child: Text("Clean Text"))
+          ],
+        ),
+      ),
+
+    );
   }
 }
